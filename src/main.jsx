db882d3e9778345798cffeb7b0939c1fe1e07245
AutomaticOrca <liangjiaying1013@gmail.com>
2024-08-08 16:40:56 +1000

    267 Connecting our Redux app with react

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,16 @@ import "./index.css";
 
 import store from "./store";
 import { createCustomer } from "./features/customers/customerSlice.js";
+import { Provider } from "react-redux";
+
 store.dispatch({ type: "account/deposit", payload: 250 });
 store.dispatch(createCustomer("Jiaying Liang", "5318698"));
 console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
